026e796d7180171838255b50752ef456c8e64fb8
Zhaohong Deng <rpredhong@icloud.com>
2020-12-07 23:03:17 +0900

    20201207_latest

---
+++ Assets/scripts/dtmfSound.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class dtmfSound : MonoBehaviour
+{
+    public static dtmfSound Instance;
+
+    public AudioClip access_2;
+    public AudioClip access_3;
+    public AudioClip access_4;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SoundEffectsHelper!");
+        }
+        Instance = this;
+    }
+
+    public void Access_2_Sound()
+    {
+        MakeSound(access_2);
+    }
+
+    public void Access_3_Sound()
+    {
+        MakeSound(access_3);
+    }
+
+    public void Access_4_Sound()
+    {
+        MakeSound(access_4);
+    }
+
+    private void MakeSound(AudioClip originalClip)
+    {
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
+}
